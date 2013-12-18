69136fbac3c5f63f47199fcfed6744455df48b25
flash2cocos2dx <litleaf@gmail.com>
2013-12-19 01:21:58 +0800

    Get the twlawplus API, get back the unique id by give search text

---
+++ lawpluscrawler.py
@@ -0,0 +1,29 @@
+#coding:utf-8
+import sys,os,time
+import requests
+import json
+import urllib
+import pprint
+
+#url = "http://httpbin.org/user-agent"
+#url = "http://www.lawplus.com.tw/rest/search/adv?querySentence=%E9%A3%9F%E5%93%81%E5%AE%89%E5%85%A8&keyword=%E9%A3%9F%E5%93%81%E5%AE%89%E5%85%A8&prevKeyword=%E9%A3%9F%E5%93%81%E5%AE%89%E5%85%A8&startDate=&endDate=&sortField=%243%24&startMoney=0&endMoney=0&startSentence=0&endSentence=0&caseNum=&courts=&rows=10000&page=1"
+#url = "http://www.pingluweb.com/rest/search/report/KSH,101,%E5%8B%9E%E4%B8%8A,16-95ae00c2-13a0-4485-b2c8-3070f7756a84"
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'
+}
+
+s = requests.Session()
+inputkeyword = "食品安全"
+encodedkeyword = urllib.quote(inputkeyword)
+print encodedkeyword
+
+url = "http://www.lawplus.com.tw/rest/search/adv?querySentence=" + encodedkeyword + "&keyword=" + encodedkeyword + "&prevKeyword=" + encodedkeyword + "&startDate=&endDate=&sortField=%243%24&startMoney=0&endMoney=0&startSentence=0&endSentence=0&caseNum=&courts=&rows=10000&page=1"
+print "url is :", url
+
+myjson = s.get(url,headers=headers).content
+objectjson = json.loads(myjson)
+rowslist = objectjson['rows']
+#pprint.pprint(objectjson)
+for i in range(0,len(rowslist)):
+    print i," ",objectjson['rows'][i]['identifier']
