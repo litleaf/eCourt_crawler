adc5fcc0f809ad37e058694645d9006bd46ba39b
jinkuen <taiwan.badboy99@gmail.com>
2013-12-20 00:37:16 +0800

    tool to query lawsuit count of each level for specific keyword from lawplus

---
+++ lp-search-level.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import requests
+import json
+import argparse
+
+def get_args():
+    description = 'Crawler for lawplus'
+    usage = "%(prog)s arg [options]"
+    parser = argparse.ArgumentParser(usage=usage)
+
+    parser.add_argument('--querySentence', default=u'', help='project name')
+    parser.add_argument('--startDate', default=u'', help='asset name')
+    parser.add_argument('--endDate', default=u'', help='asset name')
+    parser.add_argument('--sortField', default=u'%243%24', help='asset name')
+    parser.add_argument('--startMoney', default=u'', help='asset name')
+    parser.add_argument('--endMoney', default=u'', help='asset name')
+    parser.add_argument('--startSentence', default=u'', help='asset name')
+    parser.add_argument('--endSentence', default=u'', help='asset name')
+    parser.add_argument('--caseNum', default=u'', help='asset name')
+
+    args = parser.parse_args()
+    return vars(args)
+
+if sys.getdefaultencoding() != 'utf-8':
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+
+def search_level(args):
+    url = u'http://www.lawplus.com.tw/rest/search/level'
+    content = requests.get(url, params=args).content
+    json_dict = json.loads(content)
+    return json_dict
+
+if __name__ == '__main__':
+    # ./lp-search-level.py --querySentence 食品安全
+    result = search_level(get_args())
+    for r in result['response']:
+        print r['name'], r['count']
