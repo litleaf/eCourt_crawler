869c04f9ace8e567bf3f94af7baf9ee530766620
jinkuen <taiwan.badboy99@gmail.com>
2013-12-19 23:59:20 +0800

    remove header, return json dict

--- lawplus_query.py
+++ lawplus_query.py
@@ -29,20 +29,13 @@ def get_args():
     args = parser.parse_args()
     return vars(args)
 
-def lawplus_adv_query(args):
+def query(args):
     url = u'http://www.lawplus.com.tw/rest/search/adv'
-
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'
-    }
-
-    content = requests.get(url, params=args, headers=headers).content
-
+    content = requests.get(url, params=args).content
     json_dict = json.loads(content)
-    identifiers = [r['identifier'] for r in json_dict['rows']]
-    return identifiers
+    return json_dict
 
 if __name__ == '__main__':
-    lawsuits = lawplus_adv_query(get_args())
-    for lawsuit in lawsuits:
-        print lawsuit
+    result = query(get_args())
+    for row in result['rows']:
+        print row['identifier']
